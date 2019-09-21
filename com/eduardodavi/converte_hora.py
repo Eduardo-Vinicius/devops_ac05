1d6aa3c469d891e8a15848e00cb2be73177c5343
Davi Hideo Santa Rita Uemura <1900814@SALAS.AULAS>
2019-09-20 22:03:52 -0300

    Finalizado!

--- com/eduardodavi/converte_hora.py
+++ com/eduardodavi/converte_hora.py
@@ -11,14 +11,3 @@ def converteHora(hora24, minuto24):
         hora24 -= 12
     return '%02d:%02d PM' % (hora24, minuto24)
 
-
-# Entrada de dados
-continuar = 'S'
-
-while (continuar == 'S'):
-    hora = int(input('Informe o valor da hora: '))
-    minuto = int(input('Informe o valor dos minutos: '))
-
-    print(converteHora(hora, minuto))
-
-    continuar = input('Deseja continuar (S/N): ').upper()
--- com/eduardodavi/test/test_converte_hora.py
+++ com/eduardodavi/test/test_converte_hora.py
@@ -1,10 +1,24 @@
 import pytest
 from com.eduardodavi.converte_hora import converteHora
 
+
 def test_hora():
-	converte_hora = converteHora()
-	assert (converte_hora.converte_hora([12,30]) == '12:30 PM', "Should be 12:30 PM")
-    
-def test_hora():
-	converte_hora = converteHora()
-	assert (converteHora.converte_hora([43,30]) == 'Erro!' , "Insira um valor válido")
+	converte_hora = converteHora(12,30)
+	assert converte_hora == '12:30 PM', "Should be 12:30 PM"
+
+def test_seis():
+	converte_hora = converteHora(6,30)
+	assert converte_hora == '06:30 AM', "Should be 6:30 AM"
+
+def test_seis_pm():
+	converte_hora = converteHora(18,30)
+	assert converte_hora == '06:30 PM', "Should be 6:30 PM"
+
+def test_meia_noite():
+	meia_noite = converteHora(0,00)
+	assert meia_noite == '12:00 AM', "Should be 12:00 am"
+
+def test_none():
+	none_test = converteHora(34,30)
+	assert none_test == None, "Should be 12:00 am"
+
